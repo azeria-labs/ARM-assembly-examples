43b609cd9b25081017962eea5e5a5f7c52bf71ef
Azeria-labs <azeria-labs@users.noreply.github.com>
2017-07-19 12:36:39 +0300

    Create execve2.s

---
+++ execve2.s
@@ -0,0 +1,18 @@
+/* Code snipped from the tutorial 'Writing ARM Shellcode (https://azeria-labs.com/writing-arm-shellcode/), second example containing one null-byte */
+.section .text
+.global _start
+
+_start:
+        .code 32
+        add r3, pc, #1
+        bx  r3
+
+        .code 16
+        add r0, pc, #8
+        eor r1, r1, r1
+        eor r2, r2, r2
+        mov r7, #11
+        svc #1
+        mov r5, r5
+
+.ascii "/bin/sh\0"
